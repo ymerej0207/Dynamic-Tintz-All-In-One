6f64f800aeb8a8d520cd9f0be460374dd3c7ec13
Jeremy <jmcphers0207@gmail.com>
2025-08-23 18:57:59 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const N='tintz-v7.8'; self.addEventListener('install', e=>self.skipWaiting()); self.addEventListener('activate', e=>self.clients.claim());
+const N='tintz-v7.9'; self.addEventListener('install', e=>self.skipWaiting()); self.addEventListener('activate', e=>self.clients.claim());
